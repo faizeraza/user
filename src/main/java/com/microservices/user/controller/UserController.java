cf4356b0d8b08ba5845e0ad781d02303d6dcd407
Mohammad faizan <faizeraza4us@gmail.com>
2024-11-21 12:44:03 +0530

    applied communication logic for UserService to get data from Rating service Using RestTemplate

--- src/main/java/com/microservices/user/controller/UserController.java
+++ src/main/java/com/microservices/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.microservices.user.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.microservices.user.entities.Rating;
 import com.microservices.user.entities.User;
 import com.microservices.user.service.UserService;
 
@@ -24,6 +27,9 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     //create the user
     @PostMapping("/create")
     public ResponseEntity<User> createUser(@RequestBody User user) {
@@ -31,15 +37,22 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
+    @SuppressWarnings("unchecked")
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUser(@PathVariable String userId) {
         User gotUser = userService.getUser(userId);
+        ArrayList<Rating> ratings = restTemplate.getForObject("http://localhost:8083/ratings/byUser/" + userId, ArrayList.class);
+        gotUser.setRatings(ratings);
         return ResponseEntity.status(HttpStatus.FOUND).body(gotUser);
     }
 
+    @SuppressWarnings("unchecked")
     @GetMapping()
     public ResponseEntity<List<User>> getUsers() {
         List<User> users = userService.getAllUser();
+        for (User user : users) {
+            user.setRatings(restTemplate.getForObject("http://localhost:8083/ratings/byUser/" + user.getUserId(), ArrayList.class));
+        }
         return ResponseEntity.status(HttpStatus.FOUND).body(users);
     }
 
